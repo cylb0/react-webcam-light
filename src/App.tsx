16e0376996c9e0d5abac7bd1deae79de6468a701
Morgan <morgan.foucaut@greta-formations.fr>
2024-06-18 20:12:28 +0200

    feat: App displays Webcam

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import Webcam from '../lib/components/Webcam/Webcam'
+import { VideoConstraints } from '../lib/types/videoContraints'
 import './App.css'
 
 function App() {
 
+  const videoConstraints: VideoConstraints = {
+    aspectRatio: 1.77
+  }
+
   return (
     <>
-      app
+      <Webcam
+        videoConstraints={videoConstraints}
+        fullscreen
+      />
     </>
   )
 }
