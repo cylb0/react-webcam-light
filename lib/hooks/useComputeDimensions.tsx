38ab91779d3c5b81e8948843c4b0b2ae4398262b
Morgan <morgan.foucaut@greta-formations.fr>
2024-06-19 22:22:42 +0200

    feat: useComputeDimensions now handles every cases

--- lib/hooks/useComputeDimensions.tsx
+++ lib/hooks/useComputeDimensions.tsx
@@ -1,24 +1,31 @@
 import { useEffect, useState } from "react"
 import { VideoConstraints } from "../types/videoContraints"
+import { Dimensions } from "../types/webcam"
+import { base_aspect_ratio, base_width } from "../constants/webcam"
+import { AspectRatio } from "../enums/aspectRatios"
 
 export const useComputeDimensions = (videoConstraints: VideoConstraints) => {
-    const [dimensions, setDimensions] = useState<{ width: number, height: number }>({
-        width: 640,
-        height: 360
-    })
+    const [dimensions, setDimensions] = useState<Dimensions | null>(null)
 
     useEffect(() => {
-        const { aspectRatio, height: initialHeight, width: initialWidth } = videoConstraints
-        let width = initialWidth
-        let height = initialHeight
+        const { aspectRatio, height, width } = videoConstraints
+        const aspectRatioValue = AspectRatio[aspectRatio as keyof typeof AspectRatio]
 
-        if (!width && aspectRatio && height) {
-            width = Math.floor(height * aspectRatio)
-        } else if (!height && aspectRatio && width) {
-            height = Math.floor(width / aspectRatio)
+        if (height && width) {
+            setDimensions({ height, width })
+        } else if (!aspectRatio) {
+            const baseHeight = base_width * base_aspect_ratio
+            setDimensions({
+                height: height || (width ? width * base_aspect_ratio : baseHeight),
+                width: width || (height ? height / base_aspect_ratio : base_width)
+            })
+        } else if (aspectRatio) {
+            setDimensions({
+                height: height || (width ? width * aspectRatioValue : base_width * aspectRatioValue),
+                width: width || (height ? height / aspectRatioValue : base_width)
+            })
         }
 
-        setDimensions({ height: height || 360, width: width || 640 })
     }, [videoConstraints])
 
     return dimensions
