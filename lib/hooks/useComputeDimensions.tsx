0bc154e25dcd374b6c3755286adf3a01075f8e69
Morgan <morgan.foucaut@greta-formations.fr>
2024-07-09 20:32:24 +0200

    feat: updated useComputeDimensions

--- lib/hooks/useComputeDimensions.tsx
+++ lib/hooks/useComputeDimensions.tsx
@@ -2,27 +2,24 @@ import { useEffect, useState } from "react"
 import { VideoConstraints } from "../types/videoContraints"
 import { Dimensions } from "../types/webcam"
 import { base_aspect_ratio, base_width } from "../constants/webcam"
-import { AspectRatio } from "../enums/aspectRatios"
 
 export const useComputeDimensions = (videoConstraints: VideoConstraints) => {
     const [dimensions, setDimensions] = useState<Dimensions | null>(null)
 
     useEffect(() => {
         const { aspectRatio, height, width } = videoConstraints
-        const aspectRatioValue = AspectRatio[aspectRatio as keyof typeof AspectRatio]
 
         if (height && width) {
             setDimensions({ height, width })
         } else if (!aspectRatio) {
-            const baseHeight = base_width * base_aspect_ratio
             setDimensions({
-                height: height || (width ? width * base_aspect_ratio : baseHeight),
-                width: width || (height ? height / base_aspect_ratio : base_width)
+                height: height || (width ? width / base_aspect_ratio : base_width / base_aspect_ratio),
+                width: width || (height ? height * base_aspect_ratio : base_width)
             })
         } else if (aspectRatio) {
             setDimensions({
-                height: height || (width ? width * aspectRatioValue : base_width * aspectRatioValue),
-                width: width || (height ? height / aspectRatioValue : base_width)
+                height: height || (width ? width / aspectRatio : base_width / aspectRatio),
+                width: width || (height ? height * aspectRatio : base_width)
             })
         }
 
